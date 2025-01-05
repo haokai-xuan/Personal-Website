1ab5b19c66a61cb5141618bc467fa3eedf66c1dd
Haokai Xuan <haokaixuan@Haokais-MacBook-Air.local>
2025-01-04 22:03:47 -0400

    indentation fix

--- PersonalWebsite/settings.py
+++ PersonalWebsite/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),  # Static files location in the root directory
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
